bb61d95cb9494faf34b6bee5433340c3d861d9d8
Anastasiia Petrenko <petrenko.nastacia@gmail.com>
2023-06-02 16:41:36 +0300

    parameters fixed

--- src/main/java/Service/Database.java
+++ src/main/java/Service/Database.java
@@ -2,7 +2,6 @@ package Service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Database {
@@ -25,13 +24,5 @@ public class Database {
 
     public Connection getConnection() {
         return connection;
-    }
-
-    public void close() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
